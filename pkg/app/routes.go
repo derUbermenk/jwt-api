2b0fe715a2cda7102ddd8c7121a941ba9945a617
Chester Asuncion <asuncionmcv@gmail.com>
2022-06-06 21:24:18 +0800

    rename some routes and handler funcs called

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -5,14 +5,12 @@ import "github.com/gin-gonic/gin"
 func (s *Server) Routes() *gin.Engine {
 	router := s.router
 
-	public := router.Group("public/")
-	{
-		public.GET("/user/greet/:username", s.userService.PublicGreet())
-	}
+	router.POST("/signup", s.SignIn())
 
 	private := router.Group("private/")
+	private.Use(s.ValidateUser())
 	{
-		private.GET("/user/greet/:username", s.userService.PrivateGreet())
+		private.GET("/user/greet/:username", s.GetUser())
 	}
 
 	return router
